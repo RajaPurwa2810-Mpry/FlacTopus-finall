f8d51912c0b2b5afee0317270b57b52efb48947b
RajaPurwa2810-Mpry <rajapurwa.2810@gmail.com>
2026-08-30 22:28:58 +0700

    Update animasi octopus

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -237,6 +237,7 @@ const FeatureNode = ({ data, selected }) => {
 const OctopusNode = ({ data }) => {
   const { RiveComponent } = useRive({
     src: `${import.meta.env.BASE_URL}assets/4974-10065-octopus-loop.riv`,
+    animations: 'Idle',
     autoplay: true,
   });
 
